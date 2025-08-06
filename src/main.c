656af4195cd26ea483e7b6ca48efea0722c20460
Lisa <chaneliza@live.fr>
2025-08-06 22:21:53 +0200

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -12,9 +12,51 @@
 
 #include "cub3d.h"
 
-void	init_data(t_data *data)
+// void	init_data(t_data *data)
+// {
+// 	int i = 0;
+// 	data->config.texture_north = NULL;
+// 	data->config.texture_south = NULL;
+// 	data->config.texture_west = NULL;
+// 	data->config.texture_est = NULL;
+// 	data->config.floor_color = -1;
+// 	data->config.ceiling_color = -1;
+// 	data->config.texture_north_img = NULL;
+// 	data->config.texture_south_img = NULL;
+// 	data->config.texture_west_img = NULL;
+// 	data->config.texture_est_img = NULL;
+// 	data->config.last_wall_hit = 0;
+// 	data->map = NULL;
+// 	data->map_width = 0;
+// 	data->map_height = 0;
+// 	data->player.pos[0] = 0;
+// 	data->player.pos[1] = 0;
+// 	data->player.dir[0] = 0;
+// 	data->player.dir[1] = 0;
+// 	data->player.plane[0] = 0;
+// 	data->player.plane[1] = 0;
+// 	data->player.turnDirection = 0;
+// 	data->player.walkDirection = 0;
+// 	data->player.moveSpeed = 0.5; //4.0;
+// 	data->player.rotationSpeed = 0.5; //0.1;
+// 	data->player.direction = '\0';
+// 	data->player.texture_player = NULL;
+	
+// 	data->mlx = NULL;
+//     data->win = NULL;
+//     data->img = NULL;
+
+// 	while (i < 65536)
+// 	{
+//     	data->keys[i] = 0;
+//     	i++;
+// 	}
+// }
+
+#include "cub3d.h"
+
+static void	init_config_and_player(t_data *data)
 {
-	int i = 0;
 	data->config.texture_north = NULL;
 	data->config.texture_south = NULL;
 	data->config.texture_west = NULL;
@@ -37,21 +79,33 @@ void	init_data(t_data *data)
 	data->player.plane[1] = 0;
 	data->player.turnDirection = 0;
 	data->player.walkDirection = 0;
-	data->player.moveSpeed = 0.5; //4.0;
-	data->player.rotationSpeed = 0.5; //0.1;
+}
+
+static void	init_player_and_keys(t_data *data)
+{
+	int	i;
+
+	data->player.moveSpeed = 0.5;
+	data->player.rotationSpeed = 0.5;
 	data->player.direction = '\0';
 	data->player.texture_player = NULL;
-	
 	data->mlx = NULL;
-    data->win = NULL;
-    data->img = NULL;
-
+	data->win = NULL;
+	data->img = NULL;
+	i = 0;
 	while (i < 65536)
 	{
-    	data->keys[i] = 0;
-    	i++;
+		data->keys[i] = 0;
+		i++;
 	}
 }
+
+void	init_data(t_data *data)
+{
+	init_config_and_player(data);
+	init_player_and_keys(data);
+}
+
 
 int	main(int ac, char **av)
 {
